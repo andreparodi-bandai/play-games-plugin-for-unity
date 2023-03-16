09536d1e0173e88adbec0321c030e0f91f3738f7
andreparodi-bandai <63717552+andreparodi-bandai@users.noreply.github.com>
2023-03-16 11:20:29 +0100

    fix for generating Manifest in correct folder

--- Assets/Public/GooglePlayGames/com.google.play.games/Editor/GPGSUtil.cs
+++ Assets/Public/GooglePlayGames/com.google.play.games/Editor/GPGSUtil.cs
@@ -108,7 +108,7 @@ namespace GooglePlayGames.Editor
         /// <remarks>The Games SDK requires additional metadata in the AndroidManifest.xml
         ///     file. </remarks>
         private const string ManifestRelativePath =
-            "../../Plugins/Android/GooglePlayGamesManifest.androidlib/AndroidManifest.xml";
+            "Plugins/Android/GooglePlayGamesManifest.androidlib/AndroidManifest.xml";
 
         private const string RootFolderName = "com.google.play.games";
 
@@ -198,7 +198,7 @@ namespace GooglePlayGames.Editor
         ///     file. </remarks>
         private static string ManifestPath
         {
-            get { return SlashesToPlatformSeparator(Path.Combine(RootPath, ManifestRelativePath)); }
+            get { return SlashesToPlatformSeparator(Path.Combine("Assets", ManifestRelativePath)); }
         }
 
         /// <summary>
